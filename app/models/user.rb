7b0defa3cfe32349e6a6d9830af0ffa5c57134f0
Szymon Bluma <szymon.bluma@polcode.net>
2014-02-28 15:49:27 +0100

    user model, email validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ActiveRecord::Base
+
+    validates :email, :presence => true, :email => true
+
+    def status_enum
+        [['unactive',0],['active',1],['banned',2]]
+    end
+
+    rails_admin do
+        list do
+          field :id
+          field :status
+          field :email
+          field :nick
+          field :ip_login
+          field :ip_created
+          field :created_at
+          field :updated_at
+        end
+        create do
+
+            exclude_fields :activation_hash, :ip_login, :ip_created
+            
+            field :status do
+                required true
+            end
+            field :email do
+                required true
+            end
+            field :nick do
+                required true
+            end
+            field :password do
+                required true
+            end
+        end
+    end
+
 end
